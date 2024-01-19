916d985328c3daa95c6043c3a47df285913c216f
olistrik <oliverstrik@gmail.com>
2024-01-19 20:48:20 +0100

    chore: replace devenv with pure flake, remove .envrc, and add .vimignore

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "dev shell for nix people";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, flake-utils, nixpkgs }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            go_1_21
+            gopls
+          ];
+        };
+      });
+}
